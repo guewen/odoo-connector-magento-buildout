cfd88c00ca2aecab0bb3bf25dd55306b5c8d30ff
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-03-08 15:44:41 +0100

    use devtools and openerp_command_name = 'oe', use relative pathes for test scripts

--- bin/run_tests_magento.sh
+++ bin/run_tests_magento.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-./oe run-tests -dopenerp_magento7 -p8401 --addons /home/guewen/code/dev_instances/openerp_magento7/parts/webclient/addons,\
-/home/guewen/code/dev_instances/openerp_magento7/parts/addons,\
-/home/guewen/code/dev_instances/openerp_magento7/parts/openobject-extension,\
-/home/guewen/code/dev_instances/openerp_magento7/parts/e-commerce-addons,\
-/home/guewen/code/dev_instances/openerp_magento7/parts/openerp-product-attributes,\
-/home/guewen/code/dev_instances/openerp_magento7/parts/magentoerpconnect \
+./oe run-tests -dopenerp_magento7 -p8401 --addons ../parts/webclient/addons,\
+../parts/addons,\
+../parts/openobject-extension,\
+../parts/e-commerce-addons,\
+../parts/openerp-product-attributes,\
+../parts/magentoerpconnect \
 -m magentoerpconnect
