433a539422464570e5d8c22dff9fe9d39ea46d4b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-05-02 16:00:39 +0200

    [FIX] change sources to the new project on launchpad, missing argparse egg to run 'oe', changed openobject-extension to connector

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./bin/oe run-tests -dopenerp_magento7  -p8401 --addons parts/webclient/addons,parts/addons,parts/openobject-extension -m connector
+./bin/oe run-tests -dopenerp_magento7  -p8401 --addons parts/webclient/addons,parts/addons,parts/connector -m connector
--- bin/run_tests_magento.sh
+++ bin/run_tests_magento.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./bin/oe run-tests -dopenerp_magento7 -p8401 --addons parts/webclient/addons,\
 parts/addons,\
-parts/openobject-extension,\
+parts/connector,\
 parts/e-commerce-addons,\
 parts/openerp-product-attributes,\
 parts/magentoerpconnect \
